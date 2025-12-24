28c3000a520cf4d577754afee4ea018aa1f94263
Alexandre Sóstenes <alexandre@sostenes.biz>
2025-12-24 19:28:46 +0400

    Meta Pixel e GTM global - tracking em todo o sistema

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,12 @@
 // import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
 import CookieConsent from '../components/CookieConsent'
 import UTMTracker from './components/UTMTracker'
+
+// IDs de tracking globais
+const PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID || '1946279805928418'
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-KG57659R'
 
 // Temporariamente desabilitado para evitar erros de fetch do Google Fonts
 // Usando fonte do sistema como fallback
@@ -167,6 +172,41 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR" className="dark" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+
+        {/* Meta Pixel */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -199,6 +239,27 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={`${inter.variable} font-sans antialiased bg-black text-white`} suppressHydrationWarning>
+        {/* GTM noscript fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
+        {/* Meta Pixel noscript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+
         <div className="min-h-screen">
           {children}
           <CookieConsent />
